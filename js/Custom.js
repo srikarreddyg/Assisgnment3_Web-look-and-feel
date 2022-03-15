84f6f62181ecdac6e06a88cd26a3e0704fb094f3
srikarreddyg <97647392+srikarreddyg@users.noreply.github.com>
2022-03-15 03:07:12 -0400

    Delete js directory

--- js/Custom.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-    var a = document.getElementById("NavBar");
-    if (a.className === "center") {
-        a.className += " responsive";
-    } else {
-        a.className = "center";
-    }
-}
